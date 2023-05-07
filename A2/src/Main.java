24932797e62056362bfd843fe5806e8cfc001603
Christopher Kolberg <christopher.kolberg@nordakademie.de>
2023-05-07 11:06:48 +0200

    Assignment A2 improved

--- A2/src/Main.java
+++ A2/src/Main.java
@@ -1,43 +1,87 @@
 import org.apache.commons.cli.*;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Assignment 02
  * Authors: Christopher Kolberg and Maximilian Wilhelm
  */
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) {
         System.out.println("GBI - Exercise Sheet 2");
-        // For command line interface parsing you can use the commons-cli package as follows (however, feel free to use any other approach):
         Options cliOptions = new Options();
 
-        cliOptions.addOption(Option.builder().option("p1") // Short option name set to 'p1'.
-                .longOpt("parameter1") // Long option name set to 'parameter1'.
-                .hasArg(true) // Parameter has an argument.
-                .required(true) // Parameter is required.
-                .desc("CLI parameter example") // Description of the parameter.
-                .build() // Build the option.
+        cliOptions.addOption(Option.builder().option("fp1")
+                .longOpt("filepath1")
+                .hasArg(true)
+                .required(true)
+                .desc("Path to the first (query) fasta file")
+                .build()
         );
-        /*
-        The built option can be passed via cli either with -p1 SOME_VALUE or --parameter1 SOME_VALUE
-        You can add the parameter via the Run Configuration > Program Arguments field.
-         */
-        CommandLineParser parser = new DefaultParser(); // Init. parser object.
-        CommandLine params = parser.parse(cliOptions, args); // Parse built cli options from args.
-        // The option value can be retrieved as follows; By default a String type is returned. Ensure to cast to correct type.
-        String p1 = params.getOptionValue("p1");
-        // If the option was not set (and is not required), null is returned.
-        if (p1 == null) {
-            p1 = "SOME_VALUE"; // This way you can ensure to set a default value.
+
+        cliOptions.addOption(Option.builder().option("fp2")
+                .longOpt("filepath2")
+                .hasArg(true)
+                .required(true)
+                .desc("Path to the first (target) fasta file")
+                .build()
+        );
+
+        cliOptions.addOption(Option.builder().option("masc")
+                .longOpt("matchScore")
+                .hasArg(true)
+                .required(true)
+                .desc("Match score for alignment")
+                .build()
+        );
+
+        cliOptions.addOption(Option.builder().option("misc")
+                .longOpt("mismatchScore")
+                .hasArg(true)
+                .required(true)
+                .desc("Mismatch score for alignment")
+                .build()
+        );
+
+        cliOptions.addOption(Option.builder().option("gp")
+                .longOpt("gapPenalty")
+                .hasArg(true)
+                .required(true)
+                .desc("Gap penalty for alignment")
+                .build()
+        );
+
+        try (BufferedWriter bw = new BufferedWriter(new java.io.FileWriter("./resources/alignment.txt"))) {
+            // Setting up the command line parser
+            CommandLineParser parser = new DefaultParser();
+            CommandLine params = parser.parse(cliOptions, args);
+
+            // Extracting the parameters from the command line for the smith waterman aligner
+            String filenameQuery = params.getOptionValue("fp1");
+            String filenameTarget = params.getOptionValue("fp2");
+            String querySequence = SimpleFastaReader.parseFastaFile(new File("./resources/" + filenameQuery)).get(0).getSequence();
+            String targetSequence = SimpleFastaReader.parseFastaFile(new File("./resources/" + filenameTarget)).get(0).getSequence();
+            int matchScore = Integer.parseInt(params.getOptionValue("masc"));
+            int mismatchScore = Integer.parseInt(params.getOptionValue("misc"));
+            int gapPenalty = Integer.parseInt(params.getOptionValue("gp"));
+            SmithWatermanAligner smithWatermanAligner = new SmithWatermanAligner(matchScore, mismatchScore, gapPenalty, querySequence, targetSequence);
+            int optimalScore = smithWatermanAligner.alignSequences();
+            System.out.println("The optimal local alignment score for \"" + filenameQuery + "\" and \"" + filenameTarget + "\" is: " + optimalScore);
+            // Writing the results to a file
+            bw.write("The optimal local alignment score for \"" + filenameQuery + "\" and \"" + filenameTarget + "\" is: " + optimalScore);
+            bw.newLine();
+            bw.write("The optimal local alignment is: ");
+            bw.newLine();
+            bw.write(smithWatermanAligner.traceback());
+            bw.newLine();
+            bw.write("The local alignment starts at index " +
+                    smithWatermanAligner.getStartCell()[0] +
+                    " in sequence 1 and at index " +
+                    smithWatermanAligner.getStartCell()[1] +
+                    " in sequence 2.");
+        } catch (Exception e) {
+            System.err.println("Error parsing arguments: " + e.getMessage());
         }
-        // Call all functions from here and organise the output.
-        /*
-        Feel free to use the existing ./lib/SimpleFastaReader.jar or replace the file parsing with your own implementation from the last assignment.
-        Do not forget to replace the hard-coded file paths with actual cli parameters!
-         */
-        String target_sequence = SimpleFastaReader.parseFastaFile(new File("./resources/MTB_target.fasta")).get(0).getSequence();
-        String query_sequence = SimpleFastaReader.parseFastaFile(new File("./resources/MTB_query.fasta")).get(0).getSequence();
     }
 }
--- A2/src/SmithWatermanAligner.java
+++ A2/src/SmithWatermanAligner.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 /**
  * Assignment 02
- * Authors:YOUR NAMES HERE
+ * Authors: Christopher Kolberg und Maximilian Wilhelm
  */
-/*
-CLASS DOCUMENTATION HERE
-NOTE: Feel free to change the code skeleton to a static implementation, types of properties, etc., if appropriate to you.
+
+/**
+ * Class implementing the Smith-Waterman algorithm for local sequence alignment.
  */
 public class SmithWatermanAligner {
 
@@ -33,39 +35,152 @@ public class SmithWatermanAligner {
      */
     private final String s2;
 
-    // Find appropriate data structure(s) to store the alignment and traceback matrix information.
 
+    /**
+     * Alignment matrix storing the local alignment scores.
+     */
+    private final int[][] alignmentMatrix;
+    /**
+     * Traceback matrix storing the traceback information. 3-dimesional array to store the previous cell as a
+     * 2-dimensional coordinate (row, column) for each cell in the alignment matrix.
+     */
+    private final int[][][] tracebackMatrix;
+    /**
+     * Cell with the highest value in the alignment matrix.
+     */
+    private int[] bestCell = new int[2];
+    /**
+     * Cell where the local alignment starts.
+     */
+    private int[] startCell = new int[2];
+
+
+    /**
+     * Constructor for the SmithWatermanAligner class. Initializes the alignment and traceback matrices.
+     *
+     * @param matchScore    The score for a match.
+     * @param mismatchScore The score for a mismatch.
+     * @param gapPenalty    The penalty for a gap.
+     * @param s1            The first sequence to align.
+     * @param s2            The second sequence to align.
+     */
     public SmithWatermanAligner(int matchScore, int mismatchScore, int gapPenalty, String s1, String s2) {
         this.matchScore = matchScore;
         this.mismatchScore = mismatchScore;
         this.gapPenalty = gapPenalty;
         this.s1 = s1;
         this.s2 = s2;
+        this.alignmentMatrix = new int[s1.length() + 1][s2.length() + 1];
+        this.tracebackMatrix = new int[s1.length() + 1][s2.length() + 1][2];
     }
 
     /**
-     * METHOD DOCUMENTATION HERE
+     * Calculates the optimal local alignment score for the given sequences and parameters.
      *
-     * @return
+     * @return The optimal local alignment score.
      */
     public Integer alignSequences() {
+        // Optimal alignment score to be computed and returned.
         int optimalAlignmentScore = Integer.MIN_VALUE;
-        // Implement the SW DP recurrence to fill out the alignment matrix.
+
+        // Iterate over the alignment matrix and fill it in by computing the score for each cell.
+        for (int i = 0; i < s1.length() + 1; i++) {
+            for (int j = 0; j < s2.length() + 1; j++) {
+                // For the first row and column, the score is 0.
+                if (i == 0 || j == 0) {
+                    alignmentMatrix[i][j] = 0;
+                    // Convention for traceback matrix: -1 means stop traceback.
+                    tracebackMatrix[i][j][0] = -1;
+                    tracebackMatrix[i][j][1] = -1;
+                } else {
+                    // Calculate the score for the current cell and fill in the traceback matrix.
+                    calculateCell(i, j);
+                    // Update the optimal alignment score if the current cell has a higher score.
+                    if (alignmentMatrix[i][j] > optimalAlignmentScore) {
+                        optimalAlignmentScore = alignmentMatrix[i][j];
+                        bestCell = new int[]{i, j};
+                    }
+                }
+            }
+        }
         return optimalAlignmentScore;
     }
 
+
     /**
-     * METHOD DOCUMENTATION HERE
+     * Helper function to calculate the value of a cell in the alignment matrix and fill in the traceback matrix at
+     * that position.
      *
-     * @return
+     * @param i row index of the cell
+     * @param j column index of the cell
+     */
+    private void calculateCell(int i, int j) {
+        int match = alignmentMatrix[i - 1][j - 1] + (s1.charAt(i - 1) == s2.charAt(j - 1) ? matchScore : mismatchScore);
+        int delete = alignmentMatrix[i - 1][j] - gapPenalty;
+        int insert = alignmentMatrix[i][j - 1] - gapPenalty;
+
+        if (match > 0 && match >= delete && match >= insert) {
+            // Match/mismatch case
+            tracebackMatrix[i][j][0] = i - 1;
+            tracebackMatrix[i][j][1] = j - 1;
+            alignmentMatrix[i][j] = match;
+        } else if (delete > 0 && delete >= insert) {
+            // Deletion case
+            tracebackMatrix[i][j][0] = i - 1;
+            tracebackMatrix[i][j][1] = j;
+            alignmentMatrix[i][j] = delete;
+        } else if (insert > 0) {
+            // Insertion case
+            tracebackMatrix[i][j][0] = i;
+            tracebackMatrix[i][j][1] = j - 1;
+            alignmentMatrix[i][j] = insert;
+        } else {
+            // Maximum score is 0; traceback stops here.
+            tracebackMatrix[i][j][0] = -1;
+            tracebackMatrix[i][j][1] = -1;
+            alignmentMatrix[i][j] = 0;
+        }
+
+    }
+
+    /**
+     * returns the local alignment by tracing back through the traceback matrix.
+     *
+     * @return local alignment
      */
     public String traceback() {
         // Feel free to implement an own format/class to represent aligned sequences!
-        StringBuilder alignmentString = new StringBuilder();
-        // Go through the traceback matrix to reconstruct one or all optimal local alignments.
-        alignmentString.append("...");
-        return alignmentString.toString();
+        StringBuilder alignmentStringS1 = new StringBuilder();
+        StringBuilder alignmentStringS2 = new StringBuilder();
+        // Go through the traceback matrix to reconstruct one optimal local alignments.
+        // Start at the best cell and stop when the traceback reaches a cell with value -1.
+        int[] currentCell = bestCell;
+        while (!Arrays.equals(tracebackMatrix[currentCell[0]][currentCell[1]], new int[]{-1, -1})) {
+            int[] previousCell = tracebackMatrix[currentCell[0]][currentCell[1]];
+
+            if (previousCell[0] == currentCell[0] - 1 && previousCell[1] == currentCell[1] - 1) {
+                // Match/mismatch case.
+                alignmentStringS1.append(s1.charAt(currentCell[0] - 1));
+                alignmentStringS2.append(s2.charAt(currentCell[1] - 1));
+            } else if (previousCell[0] == currentCell[0] - 1) {
+                // Deletion case.
+                alignmentStringS1.append(s1.charAt(currentCell[0] - 1));
+                alignmentStringS2.append("-");
+            } else {
+                // Insertion case.
+                alignmentStringS1.append("-");
+                alignmentStringS2.append(s2.charAt(currentCell[1] - 1));
+            }
+            currentCell = previousCell;
+        }
+        // Set the start cell of the alignment.
+        this.startCell = currentCell;
+        // Reverse the alignment strings to get the correct order.
+        return alignmentStringS1.reverse() + "\n" + alignmentStringS2.reverse();
     }
 
+    public int[] getStartCell() {
+        return startCell;
+    }
 
 }
