fe2102ab8660f23002e4d50c31de086e0015cbb5
Christopher Kolberg <christopher.kolberg@nordakademie.de>
2023-05-13 16:27:20 +0200

    Done assignment 03

--- A3/src/CombineProfiles.java
+++ A3/src/CombineProfiles.java
@@ -1,18 +1,58 @@
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Assignment 03
- * Authors:YOUR NAMES HERE
+ * Authors:Christopher Kolberg, Maximilian Wilhelm
  * <p>
  * Template class for combining sequence profiles. The idea is that profile1 and profile2 both store a list of already aligned sequences.
  * The static method combineAlignedSequences should pick one sequence and align them, insert new gaps into the resp. profiles and returns
  * all new sequences in a single profile. Feel free to implement other datastructures than ArrayLists to store aligned sequences.
  */
 public class CombineProfiles {
+    private CombineProfiles() {
+    }
 
-    public static ArrayList<String> combineAlignedSequences(ArrayList<String> profile1, ArrayList<String> profile2) {
-        ArrayList<String> newProfile = new ArrayList<>();
+    // Changed method signature! Since this method is supposed to align as stated above, the method need the parameters matchScore, mismatchScore and gapPenalty.
+    public static List<String> combineAlignedSequences(List<String> profile1, List<String> profile2, int matchScore, int mismatchScore, int gapPenalty) {
+        String sequence1 = profile1.get(0);
+        String sequence2 = profile2.get(0);
+        NeedlemanWunsch nw1 = new NeedlemanWunsch(matchScore, mismatchScore,gapPenalty, sequence1, sequence2);
+        nw1.alignSequences();
+        System.out.println("Aligned the first sequence from both profiles");
+        System.out.println("Alignment score: " + nw1.getAlignmentScore());
+        System.out.println("Alignment:");
+        for(String s : nw1.getAlignedSequences()){
+            System.out.println(s);
+        }
+        System.out.println("-------------------------");
+
+
+        String alignedSequence1 = nw1.getAlignedSequences().get(0);
+        String alignedSequence2 = nw1.getAlignedSequences().get(1);
+        adjustProfile(profile1, alignedSequence1);
+        adjustProfile(profile2, alignedSequence2);
+
+
+        List<String> newProfile = new ArrayList<>();
+        newProfile.addAll(profile1);
+        newProfile.addAll(profile2);
         return newProfile;
+    }
+
+    private static void adjustProfile(List<String> profile, String alignedSequence) {
+        for(int i = 0; i < alignedSequence.length(); i++){
+            if(alignedSequence.charAt(i) == '-' ){
+                if(i >= profile.get(0).length()){
+                    profile.replaceAll(s -> s + "-");
+                }
+                if(profile.get(0).charAt(i) != '-'){
+                    for(int j = 0; j < profile.size(); j++){
+                        profile.set(j, profile.get(j).substring(0, i) + "-" + profile.get(j).substring(i));
+                    }
+                }
+            }
+        }
     }
 
 }
--- A3/src/NeedlemanWunsch.java
+++ A3/src/NeedlemanWunsch.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Assignment 03
- * Authors:YOUR NAMES HERE
+ * Authors:Christopher Kolberg, Maximilian Wilhelm
  * <p>
  * Template class for adapted Needleman-Wunsch alignment. Do not forget to comment your code ;)
  * You may want to define an appropriate data structure to store aligned sequences and their score.
@@ -20,6 +21,16 @@ public class NeedlemanWunsch {
     private final char[] sequence2;
 
     // Define appropriate data structures to store DP/traceback matrix information.
+    /**
+     * Alignment matrix storing the local alignment scores.
+     */
+    private final int[][] alignmentMatrix;
+    /**
+     * Traceback matrix storing the traceback information. 3-dimesional array to store the previous cell as a
+     * 2-dimensional coordinate (row, column) for each cell in the alignment matrix.
+     */
+    private final int[][][] tracebackMatrix;
+
 
     public NeedlemanWunsch(int matchScore, int mismatchScore, int gapPenalty, String sequence1, String sequence2) {
         this.matchScore = matchScore;
@@ -27,20 +38,114 @@ public class NeedlemanWunsch {
         this.gapPenalty = gapPenalty;
         this.sequence1 = sequence1.toCharArray();
         this.sequence2 = sequence2.toCharArray();
+        this.alignmentMatrix = new int[sequence1.length() + 1][sequence2.length() + 1];
+        this.tracebackMatrix = new int[sequence1.length() + 1][sequence2.length() + 1][2];
+
     }
 
     public void alignSequences() {
         // Fill Score- and Traceback matrix here.
+        // Iterate over the alignment matrix and fill it in by computing the score for each cell.
+        for (int i = 0; i < this.sequence1.length + 1; i++) {
+            for (int j = 0; j < this.sequence2.length + 1; j++) {
+                // For the first row and column, the score is 0.
+                if (i == 0 && j == 0) {
+                    this.alignmentMatrix[i][j] = 0;
+                    // Convention for traceback matrix: -1 is the initial value.
+                    this.tracebackMatrix[i][j][0] = -1;
+                    this.tracebackMatrix[i][j][1] = -1;
+                } else if (i == 0) {
+                    this.alignmentMatrix[i][j] = this.alignmentMatrix[i][j - 1] - this.gapPenalty;
+                    this.tracebackMatrix[i][j][0] = i;
+                    this.tracebackMatrix[i][j][1] = j - 1;
+                } else if (j == 0) {
+                    this.alignmentMatrix[i][j] = this.alignmentMatrix[i - 1][j] - this.gapPenalty;
+                    this.tracebackMatrix[i][j][0] = i - 1;
+                    this.tracebackMatrix[i][j][1] = j;
+                } else {
+                    // Calculate the score for the current cell and fill in the traceback matrix.
+                    calculateCell(i, j);
+                }
+            }
+        }
     }
+
+    /**
+     * Helper function to calculate the value of a cell in the alignment matrix and fill in the traceback matrix at
+     * that position.
+     *
+     * @param i row index of the cell
+     * @param j column index of the cell
+     */
+    private void calculateCell(int i, int j) {
+        int match;
+        if (this.sequence1[i - 1] == this.sequence2[j - 1]) {
+            if (this.sequence1[i - 1] == '-') {
+                match = this.alignmentMatrix[i - 1][j - 1];
+            } else {
+                match = this.alignmentMatrix[i - 1][j - 1] + this.matchScore;
+            }
+        } else {
+            match = this.alignmentMatrix[i - 1][j - 1] + this.mismatchScore;
+        }
+        int delete = this.alignmentMatrix[i - 1][j] - this.gapPenalty;
+        int insert = this.alignmentMatrix[i][j - 1] - this.gapPenalty;
+
+        if (match >= delete && match >= insert) {
+            // Match/mismatch case
+            this.tracebackMatrix[i][j][0] = i - 1;
+            this.tracebackMatrix[i][j][1] = j - 1;
+            this.alignmentMatrix[i][j] = match;
+        } else if (delete >= insert) {
+            // Deletion case
+            this.tracebackMatrix[i][j][0] = i - 1;
+            this.tracebackMatrix[i][j][1] = j;
+            this.alignmentMatrix[i][j] = delete;
+        } else {
+            // Insertion case
+            this.tracebackMatrix[i][j][0] = i;
+            this.tracebackMatrix[i][j][1] = j - 1;
+            this.alignmentMatrix[i][j] = insert;
+        }
+
+    }
+
 
     public int getAlignmentScore() {
         // Return the optimal global alignment score.
-        return Integer.MIN_VALUE;
+        return this.alignmentMatrix[sequence1.length][sequence2.length];
     }
 
     public ArrayList<String> getAlignedSequences() {
         // Run traceback to obtain aligned sequences.
-        return new ArrayList<>();
+        StringBuilder alignmentStringS1 = new StringBuilder();
+        StringBuilder alignmentStringS2 = new StringBuilder();
+        // Go through the traceback matrix to reconstruct one optimal global alignments.
+        // Start at the lower right cell and stop when the traceback reaches a cell with value -1.
+        int[] currentCell = new int[]{sequence1.length, sequence2.length};
+        while (!Arrays.equals(tracebackMatrix[currentCell[0]][currentCell[1]], new int[]{-1, -1})) {
+            int[] previousCell = tracebackMatrix[currentCell[0]][currentCell[1]];
+
+            if (previousCell[0] == currentCell[0] - 1 && previousCell[1] == currentCell[1] - 1) {
+                // Match/mismatch case.
+                alignmentStringS1.append(this.sequence1[currentCell[0] - 1]);
+                alignmentStringS2.append(this.sequence2[currentCell[1] - 1]);
+            } else if (previousCell[0] == currentCell[0] - 1) {
+                // Deletion case.
+                alignmentStringS1.append(this.sequence1[currentCell[0] - 1]);
+                alignmentStringS2.append("-");
+            } else {
+                // Insertion case.
+                alignmentStringS1.append("-");
+                alignmentStringS2.append(this.sequence2[currentCell[1] - 1]);
+            }
+            currentCell = previousCell;
+        }
+        // Reverse the alignment strings to get the correct order.
+        ArrayList<String> alignedStrings = new ArrayList<>();
+        alignedStrings.add(alignmentStringS1.reverse().toString());
+        alignedStrings.add(alignmentStringS2.reverse().toString());
+        return alignedStrings;
     }
 
 
