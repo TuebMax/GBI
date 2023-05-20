a54ebfba423652fd79815c1a5a884c7fafe4fab0
Christopher Kolberg <christopher.kolberg@nordakademie.de>
2023-05-20 12:21:35 +0200

    Edited A4

--- A4/src/Main.java
+++ A4/src/Main.java
@@ -1,35 +1,64 @@
 import org.apache.commons.cli.*;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Assignment 04
- * Authors:YOUR NAMES HERE
+ * Authors: Christopher Kolberg, Maximilian Wilhelm
  */
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws ParseException {
         System.out.println("GBI - Exercise Sheet 4");
-        // For command line interface parsing you can use the commons-cli package as follows (however, feel free to use any other approach):
         Options cliOptions = new Options();
-        cliOptions.addOption(Option.builder().option("p1") // Short option name set to 'p1'.
-                .longOpt("parameter1") // Long option name set to 'parameter1'.
-                .hasArg(true) // Parameter has an argument.
-                .required(true) // Parameter is required.
-                .desc("CLI parameter example") // Description of the parameter.
-                .build() // Build the option.
+
+        cliOptions.addOption(Option.builder().option("sm")
+                .longOpt("scoring-matrix")
+                .hasArg(true)
+                .required(true)
+                .desc("Scoring matrix file")
+                .build()
         );
-        /*
-        The built option can be passed via cli either with -p1 SOME_VALUE or --parameter1 SOME_VALUE
-        You can add the parameter via the Run Configuration > Program Arguments field.
-         */
-        CommandLineParser parser = new DefaultParser(); // Init. parser object.
-        CommandLine params = parser.parse(cliOptions, args); // Parse built cli options from args.
-        // The option value can be retrieved as follows; By default a String type is returned. Ensure to cast to correct type.
-        String p1 = params.getOptionValue("p1");
-        // If the option was not set (and is not required), null is returned.
-        if (p1 == null) {
-            p1 = "SOME_VALUE"; // This way you can ensure to set a default value.
+
+        cliOptions.addOption(Option.builder().option("gp")
+                .longOpt("gap-penalty")
+                .hasArg(true)
+                .required(true)
+                .desc("Gap penalty for the alignment")
+                .build()
+        );
+
+        cliOptions.addOption(Option.builder().option("sq")
+                .longOpt("sequences")
+                .hasArg(true)
+                .required(true)
+                .desc("File containing the sequences to align")
+                .build()
+        );
+        CommandLineParser parser = new DefaultParser();
+        CommandLine params = parser.parse(cliOptions, args);
+
+        String scoringMatrixFilepath;
+        int gapPenalty = 0;
+        String sequencesFilepath;
+        List<Fasta> sequences = null;
+        ScoringMatrix scoringMatrix = null;
+        // Parse the command line arguments.
+        try {
+            scoringMatrixFilepath = params.getOptionValue("sm");
+            scoringMatrix = new ScoringMatrix(scoringMatrixFilepath);
+            sequencesFilepath = params.getOptionValue("sq");
+            gapPenalty = Integer.parseInt(params.getOptionValue("gp"));
+            sequences = new FastaReader().readInFasta(sequencesFilepath);
+        } catch (Exception e) {
+            System.err.println("Error parsing arguments or reading file: " + e.getMessage());
+            System.exit(-1);
         }
+
+
+
+
+
         // Call all functions from here and organise the output.
         /*
         - Read in the fasta entries to build an MSA from.
@@ -38,5 +67,17 @@ public class Main {
         tree structure (d').
         - Run the progressive alignment by supplying the input sequences, scoring matrix, gap penalty and guide data structure.
          */
+        List<String> sequencesAsString = new ArrayList<>();
+        for (Fasta fasta : sequences) {
+            sequencesAsString.add(fasta.getSequence());
+        }
+        ProgressiveAlignment progressiveAlignment = new ProgressiveAlignment(scoringMatrix, gapPenalty);
+        List<String> combinedSequences = progressiveAlignment.alignSequences(sequencesAsString);
+
+        // Print the multiple sequence alignment.
+        System.out.println("Multiple sequence alignment:");
+        for (String s : combinedSequences) {
+            System.out.println(s);
+        }
     }
 }
--- A4/src/ProgressiveAlignment.java
+++ A4/src/ProgressiveAlignment.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Assignment 04
@@ -11,12 +14,12 @@ public class ProgressiveAlignment {
     /**
      * {@link ScoringMatrix} object used for scoring for global pairwise sequence alignment.
      */
-    private ScoringMatrix scoringMatrix;
+    private final ScoringMatrix scoringMatrix;
 
     /**
      * Positive {@link Integer} used for scoring of gaps for global pairwise sequence alignment.
      */
-    private int gapPenalty;
+    private final int gapPenalty;
 
     /**
      * Constructor object for {@link ProgressiveAlignment} class.
@@ -30,17 +33,56 @@ public class ProgressiveAlignment {
     }
 
     /**
-     * Implement progressive alignment here.
+     * Method to align a set of sequences using progressive alignment. (Changed method signature to not be static)
      *
      * @param sequencesToAlign {@link ArrayList} of {@link String} objects representing sequences to align.
-     * @param guideStructure   {@link Object} used to chose sequence order for alignment.
-     * @return {@link ArrayList} of {@link String} objects representing a MSA.
+     * @param    {@link Object} used to chose sequence order for alignment.
+     * @return {@link ArrayList} of {@link String} objects representing an MSA.
      */
-    public static ArrayList<String> alignSequences(ArrayList<String> sequencesToAlign, Object guideStructure) {
+    public List<String> alignSequences(List<String> sequencesToAlign) {
         /*
-        Implement the pseudo code from Chapter 5 (p.68) here. You will have to implement a data structure that is used to select the order in
+        Implement the pseudocode from Chapter 5 (p.68) here. You will have to implement a data structure that is used to select the order in
         which sequences are to be aligned (for this change the Type of the guideStructure parameter from Object to your implementation.
          */
+        Set<List<String>> alignments = new HashSet<>();
+
+        for (String sequence: sequencesToAlign) {
+            alignments.add(new ArrayList<>(List.of(sequence)));
+        }
+
+        while (alignments.size() > 1) {
+            List<String> subAlignment1 = alignments.iterator().next();
+            alignments.remove(subAlignment1);
+            List<String> subAlignment2 = alignments.iterator().next();
+            alignments.remove(subAlignment2);
+
+            NeedlemanWunsch nw = new NeedlemanWunsch(this.scoringMatrix, this.gapPenalty, subAlignment1.get(0), subAlignment2.get(0));
+            nw.alignSequences();
+            adjustProfile(subAlignment1, nw.getAlignedSequences().get(0));
+            adjustProfile(subAlignment2, nw.getAlignedSequences().get(1));
+            List<String> newAlignment = new ArrayList<>();
+            newAlignment.addAll(subAlignment1);
+            newAlignment.addAll(subAlignment2);
+            alignments.add(newAlignment);
+        }
+
+        return new ArrayList<>(alignments.iterator().next());
+    }
+
+    private void adjustProfile(List<String> profile, String alignedSequence) {
+        // Insert the new gaps from the aligned sequence and add them into every sequence in the profile.
+        for(int i = 0; i < alignedSequence.length(); i++){
+            if(alignedSequence.charAt(i) == '-' ){
+                if(i >= profile.get(0).length()){
+                    profile.replaceAll(s -> s + "-");
+                }
+                if(profile.get(0).charAt(i) != '-'){
+                    for(int j = 0; j < profile.size(); j++){
+                        profile.set(j, profile.get(j).substring(0, i) + "-" + profile.get(j).substring(i));
+                    }
+                }
+            }
+        }
     }
 
 }
